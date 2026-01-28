94d6e40053a2c92f6dcb662ea8c7185af2ca6da9
Victor Cavazos <victor.cavazos02@utrgv.edu>
2026-01-28 19:24:01 +0000

    Lecture 2 checkpoint 1

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -9,7 +9,36 @@
 fn main() {
     println!("Hello VICTOR CAVAZOS!");
 
-    let x = 10;
+    //let mut x = 10; // int -> int8, int16, int32, int 64, int128
+    //x += 10;
 
-    println!("5*2 = {}",x);
+    /*println!("5*2 = {}",x);
+
+    let mut result : f32 = 0.0;
+    let y:i32 = 5; //float
+    result = result + y as f32; //no implicit conversion results in error
+
+    
+    println!("{}", result);
+    
+
+    let x:i32 = 5;
+
+    let x:f32 = 1.012 + x as f32;
+
+    println!("{}",x);
+    */
+
+    
+    // Shadowing
+    let x = 5;
+    let x = x + 1;  // Creates a new variable
+    
+    // Mutation
+    let mut y = 5;
+    y = y + 1;  // Modifies the existing variable
+    
+    println!("x: {}, y: {}", x, y);
+
+
 }

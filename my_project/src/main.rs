627aceda732f8f4a34b25d4cd7d9dd24b150a856
Victor Cavazos <victor.cavazos02@utrgv.edu>
2026-02-16 19:34:13 +0000

    good enough for now Finished part one Barely got the main idea of part two done

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,9 +1,3 @@
-//Instructions:
-//1. Replace STUDENTNAME with your actual name
-//2. Compile and run the program
-//3. Compare debug vs release builds
-//4. Use stat commmand to analyze the binaries
-
 
 
 fn say_hi(x:i32){
@@ -16,11 +10,37 @@ fn double(x:i32) -> i32 {
     let y = 10;
     x*2*y
 }
-    
+
+fn fahrenheit_to_celsius(f:f64) -> f64{
+    return (f - 32.0) * (5.0/9.0) as f64;
+}
+
+fn celsius_to_fahrenheit(c:f64) -> f64{
+    return (c + 32.0) * (9.0 /5.0 ) as f64;
+}
+
+fn is_even(n: i32) -> bool{
+    return n % 2 == 0
+}
     
 
 
 fn main() {
+    let freezing_water_fahr:f64 = 32.0;
+    let mut _temp_changing:f64 = 32.0;
+    println!("{} \n", fahrenheit_to_celsius(freezing_water_fahr as f64) as f64);
+
+    while _temp_changing < 38.0{
+        println!("Number {} degrees fahrenheit converted to celsius is: {} degrees\n", _temp_changing, fahrenheit_to_celsius(_temp_changing as f64) as f64);
+        println!("Number {} degrees celsius converted to fahrenheit is: {} degrees\n", _temp_changing, celsius_to_fahrenheit(_temp_changing as f64) as f64);
+        _temp_changing += 1.0;
+    }
+
+    if is_even(10){
+        println!("10 is even");
+    }else{
+        println!("10 is not even");
+    }
     println!("Hello VICTOR CAVAZOS!");
 
     //let mut x = 10; // int -> int8, int16, int32, int 64, int128

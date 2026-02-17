f82ad2c61418d4a09d7a8897f57d4a01e398c1f3
Victor Cavazos <victor.cavazos02@utrgv.edu>
2026-02-17 04:25:25 +0000

    Done with assignment 1

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,4 +1,5 @@
 
+/*
 
 fn say_hi(x:i32){
     println!("Hi John! My favoirite number is {}", x)
@@ -10,6 +11,7 @@ fn double(x:i32) -> i32 {
     let y = 10;
     x*2*y
 }
+*/
 
 fn fahrenheit_to_celsius(f:f64) -> f64{
     return (f - 32.0) * (5.0/9.0) as f64;
@@ -22,48 +24,97 @@ fn celsius_to_fahrenheit(c:f64) -> f64{
 fn is_even(n: i32) -> bool{
     return n % 2 == 0
 }
+
+fn check_guess(guess: i32, secret: i32) -> i32{
+    if guess == secret {
+        return 0 as i32;
+    }else if guess > secret{
+        return 1 as i32;
+    }else{
+        return -1 as i32;
+    }
+}
     
 
-
+use std::io;
 fn main() {
+
+    //Not required, just made it personally easier for me to debug with a menu so I looked up how to do it
+    let mut select_program: String = String::new();
+    io::stdin().read_line(&mut select_program).expect("Unable to read Stdin");
+    
+    println!("You entered: {}", select_program);
+    if select_program.trim() == "1"{
+
+    // Part 1--------------------------------------------------------------------------------------
+    
+    //data
     let freezing_water_fahr:f64 = 32.0;
     let mut _temp_changing:f64 = 32.0;
+    //test with freezing point of water
     println!("{} \n", fahrenheit_to_celsius(freezing_water_fahr as f64) as f64);
 
+    //actual tests of function
     while _temp_changing < 38.0{
         println!("Number {} degrees fahrenheit converted to celsius is: {} degrees\n", _temp_changing, fahrenheit_to_celsius(_temp_changing as f64) as f64);
         println!("Number {} degrees celsius converted to fahrenheit is: {} degrees\n", _temp_changing, celsius_to_fahrenheit(_temp_changing as f64) as f64);
         _temp_changing += 1.0;
     }
+    }else if select_program.trim() == "2"{
 
-    if is_even(10){
-        println!("10 is even");
-    }else{
-        println!("10 is not even");
+    // Part 2--------------------------------------------------------------------------------------
+
+    //Data
+    let array_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15];
+    
+    //Actual tests with function *step 3
+    for idx in 0..(array_num.len()){
+        if is_even(array_num[idx]){
+            println!("{} is even", array_num[idx]);
+        }else{
+            print!("{} is odd ", array_num[idx]);
+            if (array_num[idx] % 5 == 0) && (array_num[idx] % 3 == 0){
+                println!("FizzBuzz");
+            }else if array_num[idx] % 3 == 0{
+                println!("Fizz");
+            }else if array_num[idx] % 5 == 0{
+                println!("Buzz");
+            }
+        }
     }
-    println!("Hello VICTOR CAVAZOS!");
 
-    //let mut x = 10; // int -> int8, int16, int32, int 64, int128
-    //x += 10;
+    //Sum of numbers *step 4
+    let mut index = 0;
+    let mut sum = 0;
+    while index < array_num.len(){
+        sum += array_num[index];
+        index += 1;
+    }
 
-    /*println!("5*2 = {}",x);
+    println!("The sum of all numbers in the array of numbers is {}...", sum);
 
-    let mut result : f32 = 0.0;
-    let y:i32 = 5; //float
-    result = result + y as f32; //no implicit conversion results in error
+    //Print the largest number in the array *step 5
+    let mut largest_num = 0;
+    for idx in array_num{
+        if idx > largest_num{
+            largest_num = idx;
+        }
+    }
+    println!("The largest number in the array is {}...", largest_num);
 
     
-    println!("{}", result);
-    
+        // Part 3-------------------------------------------------------------------------------------
+        let mut secret_number = 39;
+        let hard_coded_guesses = [2, 100, 22, 80, 39, 90];
+        let mut idx = 0;
+        while idx < hard_coded_guesses.len(){
+            if check_guess(hard_coded_guesses[idx] as i32, secret_number as i32) == 0{break;
+            }else if check_guess(hard_coded_guesses[idx] as i32, secret_number as i32) == 1{println!("Your guess of {} was too high!", idx);
+            }else if check_guess(hard_coded_guesses[idx] as i32, secret_number as i32) == -1{println!("Your guess of {} was too low!", idx);
+            }else{println!("Error found during guess checking loop...");}
+            idx += 1;
+        }
+        println!("Congratulations! It took you {} attempts to guess the secret number!", idx);
 
-    let x:i32 = 5;
-
-    let x:f32 = 1.012 + x as f32;
-
-    println!("{}",x);
-    */
-
-    say_hi(5);
-    println!("Double {} is = {}",5,double(5));
-
+    }
 }
